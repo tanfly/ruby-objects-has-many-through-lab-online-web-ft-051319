700b47542c6ebb56a66484249060b2ab81e72764
Stella McEwen <mcewen.stella@gmail.com>
2019-05-25 22:11:56 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,9 +2,15 @@ class Genre
   
   attr_accessor :name, :artist, :genre 
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @@all << self 
+  end
+  
+  def self.all 
+    @@all 
   end
   
 end
